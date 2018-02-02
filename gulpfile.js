d489a2ebf6ec1f84b0491ebe26a546adce574ce4
Shreyas Sule <suleshreyas7@gmail.com>
2018-02-01 20:01:37 -0500

    Created first gulp task called message which will Log the message.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+
+/**
+ * TOP LEVEL FUNCTIONS
+ * gulp.task :- Define tasks to perform
+ * gulp.src :- Point to files to use
+ * gulp.dest :- Point to folders to output which will contain the compiled or minified versions of file to be used in production.
+ * gulp.watch :- Watch files and folders for changes.
+ */
+
+ //Logs message
+ gulp.task("message",function(){
+     return console.log("Gulp is running...");
+ });
