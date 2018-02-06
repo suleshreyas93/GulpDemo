f3fbe2d72f9982204e25079a2d26d050540d9ad0
Shreyas Sule <suleshreyas7@gmail.com>
2018-02-06 07:32:34 -0500

    concat

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+console.log("Hello World!!"),console.log("Let's Minify using gulp!"),console.log("This is file 2"),alert("This is file 2");
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const sass = require("gulp-sass");
+const concat = require("gulp-concat");
 
 /**
  * TOP LEVEL FUNCTIONS
@@ -44,5 +45,13 @@ gulp.task("sass",function(){
         .pipe(gulp.dest("dist/css"))
 });
 
-gulp.task("default",["message","copyHtml","imageMin","minifyJS","sass"]);
+//concatenate js files
+gulp.task("scripts",function(){
+    gulp.src("src/js/*.js")
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+})
+
+gulp.task("default",["message","copyHtml","imageMin","sass","scripts"]);
  
